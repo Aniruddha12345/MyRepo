7096c0d90b5756d359cc2ed025b96acb5bd8a0fc
ASK123123 <46778262+ASK123123@users.noreply.github.com>
2019-07-02 13:50:45 +0530

    aaaa

---
+++ Java Programs/Comparator Collection/Employee.java
@@ -0,0 +1,29 @@
+package controller;
+
+public class Employee {
+	
+	public Employee(String name,Integer salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+
+	private String name;
+	private Integer salary;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getSalary() {
+		return salary;
+	}
+	public void setSalary(Integer salary) {
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", salary=" + salary + "]";
+	}
+	
+}
